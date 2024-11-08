5be4ddb3a43faa780afecfbb5b88a20953789cfb
BriA827 <brigamer13@gmail.com>
2024-11-08 07:47:53 -0500

    commit

--- fed.py
+++ fed.py
@@ -1,9 +1,16 @@
 feds = open("fed_papers.txt", "r")
 fed = feds.read().split("FEDERALIST No.")
 
-by_author = {"Hamilton":[], "Madison":[], "Jay":[], "Shared":[], "Disputed":[]}
+by_author = {"Hamilton":[], "Madison":[], "Jay":[], "Disputed":[]}
 for f in fed[1:len(fed)]:
     f = f.split(" ")
-    print(f[1].split('\n'))
-    if f[1].split('\n') == "Hamilton":
-        by_author["Hamilton"].append()
+    title = f[1].split('\n')
+    if title[2] == "HAMILTON":
+        by_author["Hamilton"].append(title[0])
+    elif title[2] == "MADISON":
+        by_author["Madison"].append(title[0])
+    elif title[2] == "JAY":
+        by_author["Jay"].append(title[0])
+    elif title[2] == "DISPUTED":
+        by_author["Disputed"].append(title[0])
+print(by_author)
