b78f7dc3baa60f55b9d1b61063e31687034bdb66
BriA827 <brigamer13@gmail.com>
2024-11-22 06:57:06 -0500

    c

--- fed.py
+++ fed.py
@@ -1,5 +1,6 @@
 import copy
 import operator
+import matplotlib.pyplot as plt
 
 feds = open("fed_papers.txt", "r")
 fed = feds.read().split("FEDERALIST No.")
@@ -22,7 +23,7 @@ for f in fed[1:len(fed)]:
 ################################################
 
 def clean(paper, indicies):
-    exceptions = ["'", "\"", ",","—",".", ";", ":", "?", "!"]
+    exceptions = ["'", "\"", ",", "—", ".", ";", ":", "?", "!"]
     paren = ["(", ")"]
     dash = "--"
     all_words = []
@@ -32,26 +33,39 @@ def clean(paper, indicies):
         full_words= []
         for i in first_pass:
             i = i.lower()
-            s
-            # for o in exceptions:
-            #     if o in i:
-            #         la = i.index(o)
-            #         if la == 0:
-            #            i = i[1:] 
-            #         else:
-            #             i = i[0:la]
-            #         full_words.append(i)
-            # for p in paren:
-            #     if p in i:
-            #         la = i.index(p)
-            #         if la == 0:
-            #             i = i[1:]
-            #             full_words.append(i)
-            #         else:
-            #             i = i[0:la]
-            #             full_words.append(i)
+            for letter in i:
+                if letter in exceptions:
+                    try:
+                        la = i.index(letter)
+                        if la == 0:
+                            i = i[1:] 
+                        else:
+                            i = i[0:la]
+                        full_words.append(i)
+                    except:
+                        continue
+
+
+                if letter in paren:
+                    try:
+                        la = i.index(letter)
+                        if la == 0:
+                            i = i[1:]
+                            full_words.append(i)
+                        else:
+                            i = i[0:la]
+                            full_words.append(i)
+                    except:
+                        continue
+
+            if dash in i:
+                la1 = i.index(dash)
+                full_words.append(i[0:la1])
+                full_words.append(i[la1+2:])
+
             else:
                 full_words.append(i)
+
         for w in full_words[5:]:
             all_words.append(w)
     return all_words
@@ -96,7 +110,7 @@ def dict_tuple(dic, k):
     sort = sorted(t, key=operator.itemgetter(k), reverse=True)
     return sort
 
-def c_sq(auth, dis):
+def c_sq(auth, dis, word_num):
     auth_d = count_words(auth) # gets the word count for auther
     dis_d = count_words(dis) #word count for disputed
     auth_tup = dict_tuple(auth_d, 1) #tuple for author words
@@ -104,13 +118,25 @@ def c_sq(auth, dis):
     merge_tup = dict_tuple(merge_dicts(auth_d, dis_d), 1) #tuple for author and disputed
 
     chi_sq = 0
-    
-    # for word in merge_tup[0:1]:
-    #     print(word)
-    #     for w in auth_tup:
-    #         print(w[0])
 
-    print(auth_d)
+    auth_share = len(auth_d) / (len(auth_d) + len(dis_d))
+    dis_share = 1 - auth_share
+    
+    for word in merge_tup[0:word_num]:
+        word_c = word[1]
+        for ao in auth_tup:
+            if ao[0] == word[0]:
+                auth_o = ao[1]
+        for do in dis_tup:
+            if do[0] == word[0]:
+                dis_o = do[1]
+
+        auth_e = auth_share * word_c
+        dis_e = dis_share * word_c
+
+        chi_sq += ((auth_e - auth_o)**2) / auth_e + ((dis_e - dis_o)**2) /dis_e
+
+    return chi_sq
 
 ###################################################
 
@@ -121,4 +147,25 @@ mad_words = clean(fed, by_author["Madison"])
 jay_words = clean(fed, by_author["Jay"])
 dis_words = clean(fed, by_author["Disputed"])
 
-c_sq(jay_words, dis_words)
+ham_chis = []
+mad_chis = []
+jay_chis = []
+scale = []
+
+for i in range(100,900,100):
+    h = c_sq(ham_words, dis_words, i)
+    m = c_sq(mad_words, dis_words, i)
+    j = c_sq(jay_words, dis_words, i)
+    ham_chis.append(h)
+    mad_chis.append(m)
+    jay_chis.append(j)
+    scale.append(i)
+
+# print(ham_chi, mad_chi, jay_chi)
+
+plt.title("Chi-Square Value VS. Numer of Words")
+plt.xlabel("Words")
+plt.ylabel("Value")
+plt.grid()
+plt.plot([scale,scale,scale], [ham_chis, mad_chis,jay_chis])
+plt.show()
