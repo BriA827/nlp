deb4ea70a223b17808d4a82814336bb3db47e307
BriA827 <brigamer13@gmail.com>
2025-01-28 11:39:34 -0500

    fix comma/par, ifx chap/quote, make sen/par, continue assignment

--- authorship.py
+++ authorship.py
@@ -10,10 +10,10 @@ def file_read(file, lines = False):
         f = f.read()
     return f
 
-def variance(data, m):
+def variance(data):
     sum = 0
     for num in data:
-        sum += (num - m)**2
+        sum += (num - 1)**2
     vari = sum/len(data)
     return vari
 
@@ -79,37 +79,60 @@ def word_par(file):
     
     return w/p
 
-def word_sen(file):
-    pass
+# def word_sen(file):
+#     pass
 
 def comma_par(file):
-    c = 0
-    p = 0
+    c_list = []
+    p_list = []
 
     for i in file.keys():
         para_count = len(file[i]["paras"])
-        p+= para_count
+        p_list.append(para_count)
+        c = 0
 
         for l in range(para_count):
             for m in file[i]["paras"][l]:
                 if "," in m:
                     c += 1
 
-    return c/p
+        c_list.append(c)
+
+    s = []
+    for i in range(len(c_list)):
+        s.append(c_list[i]/p_list[i])
+
+    return sum(s)/len(s), stand_dev(c_list)
+
+def quotes_chap(file):
+    q = 0
+    c = len(file)
+    
+    for i in file.keys():
+        for l in file[i]['paras']:
+            for w in l:
+                if "\"" in w:
+                    q += 1
+
+    return (q/2)/c
 
 #########################################
+#par comma, par sen, chap quote
 
 scarlet = file_read("Scarlet_Letter.txt", True)
 great = file_read("Great_Expectations.txt", True)
 
-tst = "Chapter I"
 scar_dict = book_dict(scarlet, "Chapter", ".")
 great_dict = book_dict(great, "Chapter", "\n")
 
 scar_wp = word_par(scar_dict)
 great_wp = word_par(great_dict)
-print(scar_wp, great_wp)
+# print(scar_wp, great_wp)
 
 scar_cp = comma_par(scar_dict)
 great_cp = comma_par(great_dict)
 print(scar_cp, great_cp)
+
+scar_qc = quotes_chap(scar_dict)
+great_qc = quotes_chap(great_dict)
+# print(scar_qc, great_qc)
